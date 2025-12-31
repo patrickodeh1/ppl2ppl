70668b022a3dcbe6d069daa266e0d211ae56a147
Patrick Odeh <vyltyan@gmail.com>
2025-12-31 23:18:42 +0100

    Update URL routing to redirect to login and include core URLs

--- ppl/urls.py
+++ ppl/urls.py
@@ -14,16 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.admin import admin_site
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('auth/', include('authentication.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', include('core.urls')),
+    path('', RedirectView.as_view(pattern_name='authentication:login', permanent=False)),
+    path('core/', include('core.urls')),
 ]
 
 # Serve media files in development
