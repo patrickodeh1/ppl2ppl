5b5bb8703cc414bc8170a10d8e9cf8920c5e3d17
Patrick Odeh <vyltyan@gmail.com>
2026-01-03 19:23:00 +0100

    Make SMTP settings configurable via environment variables for Railway port restrictions

--- ppl/settings.py
+++ ppl/settings.py
@@ -227,10 +227,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # For production, use GoDaddy cPanel email:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.ppl2pplsolutions.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'ppl2pplsolutions.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '465'))
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
 EMAIL_TIMEOUT = 10  # 10 second timeout to prevent worker hangs
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
